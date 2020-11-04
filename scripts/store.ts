f9ae203f4569868fa242d3fd11689bc363d5783e
Ernisa <ernisasulillari1@gmail.com>
2020-11-04 15:56:10 +0100

    Added methods to store

--- scripts/store.ts
+++ scripts/store.ts
@@ -1,29 +1,94 @@
-
-
-export  default class store<T> {
+export default class store<T> {
   private data: T[];
 
   constructor(...elements: T[]) {
-      this.data = elements;
+    this.data = elements;
   }
 
   add(t: T) {
-      this.data.push(t);
+    this.data.push(t);
   }
 
   remove(t: T) {
-      let index = this.data.indexOf(t);
-      if (index > -1) {
+    let index = this.data.indexOf(t);
+    if (index > -1) {
+      this.data.splice(index, 1);
+    }
+  }
+
+  removeAll() {
+    return (this.data = []);
+  }
+
+  removeAt(index: number) {
+    if (index < this.data.length && index >= -this.data.length) {
+      this.data.splice(index, 1);
+    }
+  }
+
+  removeMultiple(indexes: number[]) {
+    if (indexes.length <= this.data.length) {
+      const sorted = indexes.sort(function (a, b) {
+        return b - a;
+      });
+      const unique = new Set(sorted);
+
+      unique.forEach((index) => {
+        if (index < this.data.length) {
           this.data.splice(index, 1);
+        }
+      });
+    }
+  }
+
+  sort(compareCondition, sortOrder = "asc") {
+    for (let i = 0, len = this.data.length; i < len - 1; i++) {
+      for (let j = i + 1, len = this.data.length; j < len; j++) {
+        if (sortOrder === "asc") {
+          if (this.data[i][compareCondition] > this.data[j][compareCondition]) {
+            [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+          }
+        } else {
+          if (this.data[i][compareCondition] < this.data[j][compareCondition]) {
+            [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+          }
+        }
       }
+    }
+    return this.data;
+  }
+
+  findItem(key: string, value: any) {
+    let item = {};
+    for (let i = 0, len = this.data.length; i < len; i++) {
+      item[this.data[i][key]] = this.data[i];
+    }
+    return item[value];
+  }
+
+  findIndex(key: string, value: any) {
+    return this.data.findIndex((item) => item[key] === value);
+  }
+
+  filterBy(key: string, value: any) {
+    return this.data.filter(function (item) {
+      return item[key] === value;
+    });
   }
 
   getStore(): T[] {
-      return this.data;
+    return this.data;
+  }
+
+  getAt(index: number) {
+    if (index > this.data.length) {
+      return;
+    }
+    return this.data[index];
   }
 }
 
-console.log()
+console.log();
 
 // export default class Store {
 
@@ -36,7 +101,6 @@ console.log()
 //     constructor(config: any) {
 //         //to do
 //     }
-
 
 //     public removeAt (index: number) {
 //         if (index < this.data.length && index >= 0) {
