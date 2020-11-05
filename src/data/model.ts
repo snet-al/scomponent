b7402bbaeedc42f532fb4d2c7ec0afec17a86826
Ernisa <ernisasulillari1@gmail.com>
2020-11-05 12:12:06 +0100

    Comments resolved

--- src/data/model.ts
+++ src/data/model.ts
@@ -1,29 +1,27 @@
 export default class Model {
   private translateFields = {};
 
-  public factory(data: any): any {
+  public factory(data: Object): any {
+    if (!data) {
+      return;
+    }
     for (let key in data) {
       if (data.hasOwnProperty(key)) {
         let rawData = data[key];
         key = this.translate(key);
-        if (!this.hasOwnProperty(key) || typeof rawData === "undefined") {
-          continue;
+        if (this.hasOwnProperty(key) || typeof rawData !== "undefined") {
+          let goDeep = this[key] instanceof Model;
+          if (goDeep) {
+            this[key].factory(rawData);
+          }
+          (<any>this)[key] = rawData;
         }
-        let goDeep = this[key] instanceof Model;
-        if (goDeep) {
-          this[key].factory(rawData);
-          continue;
-        }
-        (<any>this)[key] = rawData;
       }
     }
   }
 
   public translate(key: string) {
-    if (!this.translateFields) {
-      return key;
-    }
-    if (!this.translateFields[key]) {
+    if (!this.translateFields || !this.translateFields[key]) {
       return key;
     }
     return this.translateFields[key];
